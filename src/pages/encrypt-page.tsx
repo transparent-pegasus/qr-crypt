3a10a6ca462f1f7abcd4d0a7e68a13b5e9074cc3
transparent-pegasus <258247454+transparent-pegasus@users.noreply.github.com>
2026-08-28 04:15:44 +0800

    refactor: read algorithm labels from catalogs

--- src/pages/encrypt-page.tsx
+++ src/pages/encrypt-page.tsx
@@ -40,7 +40,6 @@ import {
   type EncryptMessageRequest,
 } from "@/features/encrypt-message"
 import {
-  ALGORITHM_LABELS,
   formatDateTime,
   formatSuggestedDate,
 } from "@/features/presentation"
@@ -360,7 +359,7 @@ export function EncryptPage() {
           <SelectContent>
             {algorithms.map((option) => (
               <SelectItem key={option} value={option}>
-                {ALGORITHM_LABELS[language][option]}
+                {t(`algorithm.${option}`)}
               </SelectItem>
             ))}
           </SelectContent>
