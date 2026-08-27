9f2d4ef5e29a75bee623b39ae0fa4e383bb67a21
transparent-pegasus <258247454+transparent-pegasus@users.noreply.github.com>
2026-08-28 01:41:44 +0800

    test: remove unused database fakes

--- tests/ui/helpers/module-mocks/database.ts
+++
@@ -1,9 +0,0 @@
-import { vi } from "vitest"
-import * as fakes from "../fakes/database"
-
-vi.mock("@/storage/database", async (importOriginal) => ({
-  ...(await importOriginal<typeof import("@/storage/database")>()),
-  getDb: fakes.getDb,
-  closeDb: fakes.closeDb,
-  deleteEntireDatabase: fakes.deleteEntireDatabase,
-}))
